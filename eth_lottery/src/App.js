40ce423278f3bd156a8b5ffc522be88878e384f3
rogersanick <rogers.a.nick@gmail.com>
2018-09-01 21:37:29 -0400

    Major progress on lottery front-end - replaced provider on project instance of web3 (1.0.0+ to make use of promises) - enabled users to enter contract with set amount of ETH

--- eth_lottery/src/App.js
+++ eth_lottery/src/App.js
@@ -1,18 +1,65 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import web3 from './web3.js';
+import lottery from './lottery.js';
 class App extends Component {
+
+  state = {
+    manager: '',
+    players: [],
+    balance: '',
+    value:'',
+    message: ''
+  };
+  
+  async componentDidMount() {
+    // When using metamask provider (and instance of web3), default provider is already set.
+    const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.returnPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+
+    this.setState({
+      manager,
+      players,
+      balance,
+    });
+
+  }
+
+  async handleSubmit(e) {
+    e.preventDefault();
+    const accounts = await web3.eth.getAccounts();
+    this.setState({
+      message: 'Waiting on transaction success...'
+    });
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(this.state.value, 'ether')
+    });
+    this.setState({
+      message: `You have successfully entered the lottery with ${this.state.value} ETH`
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+      <div>
+        <h2>Lottery Contract</h2>
+        <p>This contract is managed by {this.state.manager}.
+          There are currently {this.state.players.length} entered, competing to win {web3.utils.fromWei(this.state.balance, 'ether')} ETH.
         </p>
+        <hr/>
+        <form onSubmit = {e => this.handleSubmit(e)}>
+          <h4>Want to try your luck?</h4>
+          <div>
+            <label>Amount of ETH to enter: </label>
+            <input value = {this.state.value} onChange = {(e) => { this.setState({ value: e.target.value }) }}></input>
+          </div>
+          <button>Enter</button>
+        </form>
+        <hr/>
+        <h1>{this.state.message}</h1>
       </div>
     );
   }
---
+++ eth_lottery/src/lottery.js
@@ -0,0 +1,7 @@
+import web3 from './web3.js';
+
+const address = '0x679e9475131ee6C6C3503B2Bf06c6df8224073c6';
+
+const abi = [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"returnPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}];
+
+export default new web3.eth.Contract(abi, address);
